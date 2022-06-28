54437fda5d3d2844ec5886d34d1c896d18509ffd
blitzpc17 <remizeroaw117@gmail.com>
2022-06-28 07:50:10 -0500

    Agregar archivos de proyecto.

---
+++ reporte/Form1.Designer.cs
@@ -0,0 +1,67 @@
+﻿namespace reporte
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnLeer = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.SuspendLayout();
+            // 
+            // btnLeer
+            // 
+            this.btnLeer.Location = new System.Drawing.Point(108, 308);
+            this.btnLeer.Name = "btnLeer";
+            this.btnLeer.Size = new System.Drawing.Size(75, 23);
+            this.btnLeer.TabIndex = 0;
+            this.btnLeer.Text = "button1";
+            this.btnLeer.UseVisualStyleBackColor = true;
+            this.btnLeer.Click += new System.EventHandler(this.btnLeer_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnLeer);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnLeer;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+    }
+}
+
---
+++ reporte/Form1.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Excel = Microsoft.Office.Interop.Excel;
+
+namespace reporte
+{
+    public partial class Form1 : Form
+    {
+        Excel.Application ReporteApp = default(Excel.Application);
+        Excel.Workbook Libro = default(Excel.Workbook);
+        Excel.Worksheet HojaReporte = default(Excel.Worksheet);
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnLeer_Click(object sender, EventArgs e)
+        {
+            openFileDialog1 = new OpenFileDialog();
+            openFileDialog1.ShowDialog(this);
+            string ruta = openFileDialog1.FileName;
+            LeerTxt(ruta);
+        }
+
+        private void LeerTxt(string ruta)
+        {
+            ReporteApp = new Excel.Application();
+            ReporteApp.Visible = true;
+
+            Libro = ReporteApp.Workbooks.Add();
+            HojaReporte = Libro.Worksheets[1];
+            HojaReporte.Visible = Excel.XlSheetVisibility.xlSheetVisible;
+            HojaReporte.Activate();
+
+
+            int numeroRows = System.IO.File.ReadLines(ruta).Count();
+            int countRows = 0;
+            foreach ( string linea in System.IO.File.ReadLines(ruta))
+            {
+                string ln = linea;
+                if (countRows == 0)
+                {
+                    HojaReporte.Cells[countRows + 1, "A"] = "Cuenta";
+                    HojaReporte.Cells[countRows + 1, "B"] = "Descripción";
+                    HojaReporte.Cells[countRows + 1, "C"] = "Saldo Ant.";
+                    HojaReporte.Cells[countRows + 1, "D"] = "Cargos";
+                    HojaReporte.Cells[countRows + 1, "E"] = "Abonos";
+                    HojaReporte.Cells[countRows + 1, "F"] = "Saldo";
+                }
+                else if (countRows >= 2)
+                {
+                    String[] renglon = ln.ToString().Split(' ');
+                    string descrpcion = "";
+                    int columnasImportes = 0;
+                    for (int i = 0; i < renglon.Length; i++)
+                    {
+                        decimal salida = 0;
+
+                        if (i == 0)
+                        {
+                            HojaReporte.Cells[countRows, "A"] = renglon[i].ToString();
+                        }
+                        else if (!string.IsNullOrWhiteSpace(renglon[i]) && !decimal.TryParse(renglon[i], out salida))
+                        {
+                            descrpcion += Convert.ToString(renglon[i]) + " ";
+                            HojaReporte.Cells[countRows, "B"] = descrpcion;
+                        }
+                        else if (decimal.TryParse(renglon[i], out salida))
+                        {
+                            columnasImportes++;
+                            if (columnasImportes > 4) break;
+
+                            switch (columnasImportes)
+                            {
+                                case 1:
+                                    HojaReporte.Cells[countRows, "C"] = salida;
+                                    break;
+                                case 2:
+                                    HojaReporte.Cells[countRows, "D"] = salida;
+                                    break;
+                                case 3:
+                                    HojaReporte.Cells[countRows, "E"] = salida;
+                                    break;
+                                case 4:
+                                    HojaReporte.Cells[countRows, "F"] = salida;
+                                    break;
+                            }
+
+                        }
+                    }
+                }
+                else if (countRows + 3 == numeroRows ) break;
+                countRows++;
+               
+            
+
+
+                Console.WriteLine(linea);
+            }
+        }
+    }
+}
