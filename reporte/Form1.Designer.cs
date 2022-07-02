7f171e1a650582b852f5d555e8ee9439c08c67bc
blitzpc17 <remizeroaw117@gmail.com>
2022-07-02 00:05:27 -0500

    Se respaldan el proyecto de generacion de reporte

--- reporte/Form1.Designer.cs
+++ reporte/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace reporte
 {
-    partial class Form1
+    partial class frmReporte
     {
         /// <summary>
         /// Variable del diseñador necesaria.
@@ -28,40 +28,86 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.btnLeer = new System.Windows.Forms.Button();
+            this.btnCargarArchivo = new System.Windows.Forms.Button();
             this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btnSalir = new System.Windows.Forms.Button();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
-            // btnLeer
+            // btnCargarArchivo
             // 
-            this.btnLeer.Location = new System.Drawing.Point(108, 308);
-            this.btnLeer.Name = "btnLeer";
-            this.btnLeer.Size = new System.Drawing.Size(75, 23);
-            this.btnLeer.TabIndex = 0;
-            this.btnLeer.Text = "button1";
-            this.btnLeer.UseVisualStyleBackColor = true;
-            this.btnLeer.Click += new System.EventHandler(this.btnLeer_Click);
+            this.btnCargarArchivo.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnCargarArchivo.Location = new System.Drawing.Point(6, 28);
+            this.btnCargarArchivo.Name = "btnCargarArchivo";
+            this.btnCargarArchivo.Size = new System.Drawing.Size(241, 36);
+            this.btnCargarArchivo.TabIndex = 0;
+            this.btnCargarArchivo.Text = "Cargar Archivo";
+            this.btnCargarArchivo.UseVisualStyleBackColor = true;
+            this.btnCargarArchivo.Click += new System.EventHandler(this.btnLeer_Click);
             // 
             // openFileDialog1
             // 
             this.openFileDialog1.FileName = "openFileDialog1";
             // 
-            // Form1
+            // groupBox1
+            // 
+            this.groupBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox1.Controls.Add(this.btnSalir);
+            this.groupBox1.Controls.Add(this.btnCargarArchivo);
+            this.groupBox1.Location = new System.Drawing.Point(13, 13);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(253, 128);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Acciones";
+            // 
+            // btnSalir
+            // 
+            this.btnSalir.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnSalir.Location = new System.Drawing.Point(6, 70);
+            this.btnSalir.Name = "btnSalir";
+            this.btnSalir.Size = new System.Drawing.Size(241, 36);
+            this.btnSalir.TabIndex = 1;
+            this.btnSalir.Text = "Salir";
+            this.btnSalir.UseVisualStyleBackColor = true;
+            this.btnSalir.Click += new System.EventHandler(this.btnSalir_Click);
+            // 
+            // backgroundWorker1
+            // 
+            this.backgroundWorker1.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker1_DoWork);
+            this.backgroundWorker1.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.backgroundWorker1_RunWorkerCompleted);
+            // 
+            // frmReporte
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.btnLeer);
-            this.Name = "Form1";
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(278, 153);
+            this.Controls.Add(this.groupBox1);
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(294, 192);
+            this.MinimizeBox = false;
+            this.MinimumSize = new System.Drawing.Size(294, 192);
+            this.Name = "frmReporte";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Generador de Reporte";
+            this.groupBox1.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
 
         #endregion
 
-        private System.Windows.Forms.Button btnLeer;
+        private System.Windows.Forms.Button btnCargarArchivo;
         private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btnSalir;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
     }
 }
 
--- reporte/Form1.cs
+++ reporte/Form1.cs
@@ -11,14 +11,15 @@ using Excel = Microsoft.Office.Interop.Excel;
 
 namespace reporte
 {
-    public partial class Form1 : Form
+    public partial class frmReporte : Form
     {
         Excel.Application ReporteApp = default(Excel.Application);
         Excel.Workbook Libro = default(Excel.Workbook);
         Excel.Worksheet HojaReporte = default(Excel.Worksheet);
+        private string ruta = "";
 
 
-        public Form1()
+        public frmReporte()
         {
             InitializeComponent();
         }
@@ -27,8 +28,10 @@ namespace reporte
         {
             openFileDialog1 = new OpenFileDialog();
             openFileDialog1.ShowDialog(this);
-            string ruta = openFileDialog1.FileName;
-            LeerTxt(ruta);
+            ruta = openFileDialog1.FileName;
+            btnCargarArchivo.Enabled = false;
+            backgroundWorker1.RunWorkerAsync();
+            //LeerTxt(ruta);
         }
 
         private void LeerTxt(string ruta)
@@ -43,36 +46,72 @@ namespace reporte
 
 
             int numeroRows = System.IO.File.ReadLines(ruta).Count();
-            int countRows = 0;
+            int countRows = 1;
             foreach ( string linea in System.IO.File.ReadLines(ruta))
             {
                 string ln = linea;
-                if (countRows == 0)
+                if (countRows == 1)
                 {
-                    HojaReporte.Cells[countRows + 1, "A"] = "Cuenta";
-                    HojaReporte.Cells[countRows + 1, "B"] = "Descripción";
-                    HojaReporte.Cells[countRows + 1, "C"] = "Saldo Ant.";
-                    HojaReporte.Cells[countRows + 1, "D"] = "Cargos";
-                    HojaReporte.Cells[countRows + 1, "E"] = "Abonos";
-                    HojaReporte.Cells[countRows + 1, "F"] = "Saldo";
+                    HojaReporte.Cells[countRows, "A"] = "Cuenta";
+                    HojaReporte.Cells[countRows, "B"] = "Sub Cta";
+                    HojaReporte.Cells[countRows, "C"] = "Concepto";
+                    HojaReporte.Cells[countRows, "D"] = "Saldo según balanza";
+                    HojaReporte.Cells[countRows, "E"] = "Saldos s/Audit.";
+                    HojaReporte.Cells[countRows, "F"] = "Diferencia";
+                    HojaReporte.Cells[countRows, "G"] = "Marca Auditoria";
                 }
-                else if (countRows >= 2)
+                else if (countRows == numeroRows - 4) break;
+                else if (countRows > 2)
                 {
                     String[] renglon = ln.ToString().Split(' ');
                     string descrpcion = "";
                     int columnasImportes = 0;
+                    decimal salida = 0;
                     for (int i = 0; i < renglon.Length; i++)
                     {
+                        if (i == 0 && decimal.TryParse(renglon[i].ToString(), out salida))
+                        {
+                            columnasImportes = 0;
+                            HojaReporte.Cells[countRows-1, "A"] = renglon[i].ToString();
+                        }
+                        else if(i == 0 )
+                        {
+                            HojaReporte.Cells[countRows - 1, "B"] = renglon[i].ToString();
+                        }else
+                        {
+                            if(decimal.TryParse(renglon[i].ToString(), out salida))
+                            {
+                                columnasImportes++;
+                                if (columnasImportes == 4)
+                                {
+                                    HojaReporte.Cells[countRows - 1, "D"] = renglon[i].ToString();
+                                    HojaReporte.Cells[countRows - 1, "E"] = renglon[i].ToString();
+                                }
+                            }
+                            else
+                            {
+                                if (!string.IsNullOrWhiteSpace(renglon[i].ToString()))
+                                {
+                                    descrpcion += Convert.ToString(renglon[i]) + " ";
+                                    HojaReporte.Cells[countRows - 1, "C"] = descrpcion;
+                                }
+                            }
+
+                        }
+
+
+
+                        /*
                         decimal salida = 0;
 
                         if (i == 0)
                         {
-                            HojaReporte.Cells[countRows, "A"] = renglon[i].ToString();
+                            HojaReporte.Cells[countRows-1, "A"] = renglon[i].ToString();
                         }
                         else if (!string.IsNullOrWhiteSpace(renglon[i]) && !decimal.TryParse(renglon[i], out salida))
                         {
                             descrpcion += Convert.ToString(renglon[i]) + " ";
-                            HojaReporte.Cells[countRows, "B"] = descrpcion;
+                            HojaReporte.Cells[countRows-1, "B"] = descrpcion;
                         }
                         else if (decimal.TryParse(renglon[i], out salida))
                         {
@@ -82,23 +121,25 @@ namespace reporte
                             switch (columnasImportes)
                             {
                                 case 1:
-                                    HojaReporte.Cells[countRows, "C"] = salida;
+                                    HojaReporte.Cells[countRows-1, "C"] = salida;
                                     break;
                                 case 2:
-                                    HojaReporte.Cells[countRows, "D"] = salida;
+                                    HojaReporte.Cells[countRows-1, "D"] = salida;
                                     break;
                                 case 3:
-                                    HojaReporte.Cells[countRows, "E"] = salida;
+                                    HojaReporte.Cells[countRows-1, "E"] = salida;
                                     break;
                                 case 4:
-                                    HojaReporte.Cells[countRows, "F"] = salida;
+                                    HojaReporte.Cells[countRows-1, "F"] = salida;
                                     break;
                             }
+                        */
 
-                        }
+
+
                     }
                 }
-                else if (countRows + 3 == numeroRows ) break;
+               
                 countRows++;
                
             
@@ -106,6 +147,23 @@ namespace reporte
 
                 Console.WriteLine(linea);
             }
+        }
+
+        private void btnSalir_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
+        {
+            LeerTxt(ruta);
+        }
+
+        private void backgroundWorker1_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            MessageBox.Show("Reporte generado correctamente.", "Aviso", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            btnCargarArchivo.Enabled = true;
+            this.Focus();
         }
     }
 }
