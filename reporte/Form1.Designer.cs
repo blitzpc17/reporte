d1aedc1806a3aafab8459b5d12cad9dc4c5b20f7
blitzpc17 <remizeroaw117@gmail.com>
2022-07-26 13:58:36 -0500

    SE CAMBIO LA FORMA DE LEER EL XLM Y MODIFICAR SOBRE EL MISMO EXCEL QUE IMPORTEN

--- reporte/Form1.Designer.cs
+++ reporte/Form1.Designer.cs
@@ -31,8 +31,16 @@
             this.btnCargarArchivo = new System.Windows.Forms.Button();
             this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.buttonCargarExcel = new System.Windows.Forms.Button();
+            this.txtHoja = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtFilas = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
             this.btnSalir = new System.Windows.Forms.Button();
             this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.txtColumna = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
             this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -40,11 +48,11 @@
             // 
             this.btnCargarArchivo.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.btnCargarArchivo.Location = new System.Drawing.Point(6, 28);
+            this.btnCargarArchivo.Location = new System.Drawing.Point(6, 112);
             this.btnCargarArchivo.Name = "btnCargarArchivo";
-            this.btnCargarArchivo.Size = new System.Drawing.Size(241, 36);
-            this.btnCargarArchivo.TabIndex = 0;
-            this.btnCargarArchivo.Text = "Cargar Archivo";
+            this.btnCargarArchivo.Size = new System.Drawing.Size(310, 36);
+            this.btnCargarArchivo.TabIndex = 30;
+            this.btnCargarArchivo.Text = "Cargar archivo cuentas";
             this.btnCargarArchivo.UseVisualStyleBackColor = true;
             this.btnCargarArchivo.Click += new System.EventHandler(this.btnLeer_Click);
             // 
@@ -57,23 +65,78 @@
             this.groupBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox1.Controls.Add(this.txtColumna);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.buttonCargarExcel);
+            this.groupBox1.Controls.Add(this.txtHoja);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.txtFilas);
+            this.groupBox1.Controls.Add(this.label1);
             this.groupBox1.Controls.Add(this.btnSalir);
             this.groupBox1.Controls.Add(this.btnCargarArchivo);
             this.groupBox1.Location = new System.Drawing.Point(13, 13);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(253, 128);
-            this.groupBox1.TabIndex = 1;
+            this.groupBox1.Size = new System.Drawing.Size(322, 251);
+            this.groupBox1.TabIndex = 100;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Acciones";
+            // 
+            // buttonCargarExcel
+            // 
+            this.buttonCargarExcel.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonCargarExcel.Location = new System.Drawing.Point(6, 154);
+            this.buttonCargarExcel.Name = "buttonCargarExcel";
+            this.buttonCargarExcel.Size = new System.Drawing.Size(310, 36);
+            this.buttonCargarExcel.TabIndex = 40;
+            this.buttonCargarExcel.Text = "Cargar Excel";
+            this.buttonCargarExcel.UseVisualStyleBackColor = true;
+            this.buttonCargarExcel.Click += new System.EventHandler(this.buttonCargarExcel_Click);
+            // 
+            // txtHoja
+            // 
+            this.txtHoja.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtHoja.Location = new System.Drawing.Point(124, 28);
+            this.txtHoja.Name = "txtHoja";
+            this.txtHoja.Size = new System.Drawing.Size(192, 22);
+            this.txtHoja.TabIndex = 10;
+            // 
+            // label2
+            // 
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(8, 28);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(110, 23);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Hoja:";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // txtFilas
+            // 
+            this.txtFilas.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtFilas.Location = new System.Drawing.Point(124, 56);
+            this.txtFilas.Name = "txtFilas";
+            this.txtFilas.Size = new System.Drawing.Size(192, 22);
+            this.txtFilas.TabIndex = 20;
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(8, 56);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(110, 23);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "En fila:";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
             // 
             // btnSalir
             // 
             this.btnSalir.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.btnSalir.Location = new System.Drawing.Point(6, 70);
+            this.btnSalir.Location = new System.Drawing.Point(6, 195);
             this.btnSalir.Name = "btnSalir";
-            this.btnSalir.Size = new System.Drawing.Size(241, 36);
-            this.btnSalir.TabIndex = 1;
+            this.btnSalir.Size = new System.Drawing.Size(310, 36);
+            this.btnSalir.TabIndex = 50;
             this.btnSalir.Text = "Salir";
             this.btnSalir.UseVisualStyleBackColor = true;
             this.btnSalir.Click += new System.EventHandler(this.btnSalir_Click);
@@ -83,20 +146,37 @@
             this.backgroundWorker1.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker1_DoWork);
             this.backgroundWorker1.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.backgroundWorker1_RunWorkerCompleted);
             // 
+            // txtColumna
+            // 
+            this.txtColumna.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtColumna.Location = new System.Drawing.Point(124, 84);
+            this.txtColumna.Name = "txtColumna";
+            this.txtColumna.Size = new System.Drawing.Size(192, 22);
+            this.txtColumna.TabIndex = 52;
+            // 
+            // label3
+            // 
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(8, 84);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(110, 23);
+            this.label3.TabIndex = 51;
+            this.label3.Text = "En columna:";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
             // frmReporte
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(278, 153);
+            this.ClientSize = new System.Drawing.Size(347, 276);
             this.Controls.Add(this.groupBox1);
             this.MaximizeBox = false;
-            this.MaximumSize = new System.Drawing.Size(294, 192);
             this.MinimizeBox = false;
-            this.MinimumSize = new System.Drawing.Size(294, 192);
             this.Name = "frmReporte";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Generador de Reporte";
             this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -108,6 +188,14 @@
         private System.Windows.Forms.GroupBox groupBox1;
         private System.Windows.Forms.Button btnSalir;
         private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.TextBox txtFilas;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtHoja;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button buttonCargarExcel;
+        private System.Windows.Forms.TextBox txtColumna;
+        private System.Windows.Forms.Label label3;
     }
 }
 
--- reporte/Form1.cs
+++ reporte/Form1.cs
@@ -1,22 +1,28 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.ComponentModel;
-using System.Data;
-using System.Drawing;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
-using Excel = Microsoft.Office.Interop.Excel;
+using Microsoft.Office.Interop.Excel;
+using Application = Microsoft.Office.Interop.Excel.Application;
+using Workbook = Microsoft.Office.Interop.Excel.Workbook;
+using Worksheet = Microsoft.Office.Interop.Excel.Worksheet;
+using Borders = Microsoft.Office.Interop.Excel.Borders;
+
+using DocumentFormat.OpenXml;
+using DocumentFormat.OpenXml.Spreadsheet;
+using SpreadsheetLight;
 
 namespace reporte
 {
     public partial class frmReporte : Form
     {
-        Excel.Application ReporteApp = default(Excel.Application);
-        Excel.Workbook Libro = default(Excel.Workbook);
-        Excel.Worksheet HojaReporte = default(Excel.Worksheet);
-        private string ruta = "";
+        Application ReporteApp;
+        Workbook Libro;
+        Worksheet HojaReporte;
+        private string ruta = "", rutaExcel ="";
+        private int countRows = 0;
+        bool excelCargado = false;
+        bool archivoCargado = false;
 
 
         public frmReporte()
@@ -26,39 +32,37 @@ namespace reporte
 
         private void btnLeer_Click(object sender, EventArgs e)
         {
-            openFileDialog1 = new OpenFileDialog();
-            openFileDialog1.ShowDialog(this);
-            ruta = openFileDialog1.FileName;
-            btnCargarArchivo.Enabled = false;
-            backgroundWorker1.RunWorkerAsync();
-            //LeerTxt(ruta);
+            ImportarArchivoCuentas();
         }
 
-        private void LeerTxt(string ruta)
+        private void LeerTxt(string ruta, string rutaExcel)
         {
-            ReporteApp = new Excel.Application();
-            ReporteApp.Visible = true;
+            ReporteApp = new Application();
+            Libro = ReporteApp.Workbooks.Open(rutaExcel);
+            HojaReporte = Libro.Worksheets[0];
+            
+            countRows = 0;
 
-            Libro = ReporteApp.Workbooks.Add();
-            HojaReporte = Libro.Worksheets[1];
-            HojaReporte.Visible = Excel.XlSheetVisibility.xlSheetVisible;
-            HojaReporte.Activate();
 
+            int totalRows = HojaReporte.Rows.Count;
+            int totalCols = HojaReporte.Columns.Count;
+
+            countRows = totalRows + 5;
 
             int numeroRows = System.IO.File.ReadLines(ruta).Count();
-            int countRows = 1;
+            
             foreach ( string linea in System.IO.File.ReadLines(ruta))
             {
                 string ln = linea;
                 if (countRows == 1)
                 {
-                    HojaReporte.Cells[countRows, "A"] = "Cuenta";
-                    HojaReporte.Cells[countRows, "B"] = "Sub Cta";
-                    HojaReporte.Cells[countRows, "C"] = "Concepto";
-                    HojaReporte.Cells[countRows, "D"] = "Saldo según balanza";
-                    HojaReporte.Cells[countRows, "E"] = "Saldos s/Audit.";
-                    HojaReporte.Cells[countRows, "F"] = "Diferencia";
-                    HojaReporte.Cells[countRows, "G"] = "Marca Auditoria";
+                    HojaReporte.Cells[countRows, "A"] = "CUENTA";
+                    HojaReporte.Cells[countRows, "B"] = "SUBCTA";
+                    HojaReporte.Cells[countRows, "C"] = "CONCEPTO";
+                    HojaReporte.Cells[countRows, "D"] = "SALDO SEGÚN BALANZA";
+                    HojaReporte.Cells[countRows, "E"] = "SALDO S/ AUDITORIA";
+                    HojaReporte.Cells[countRows, "F"] = "DIFERENCIA";
+                    HojaReporte.Cells[countRows, "G"] = "MARCA DE AUDITORIA";
                 }
                 else if (countRows == numeroRows - 4) break;
                 else if (countRows > 2)
@@ -93,50 +97,11 @@ namespace reporte
                                 if (!string.IsNullOrWhiteSpace(renglon[i].ToString()))
                                 {
                                     descrpcion += Convert.ToString(renglon[i]) + " ";
-                                    HojaReporte.Cells[countRows - 1, "C"] = descrpcion;
+                                    HojaReporte.Cells[countRows - 1, "C"] = (descrpcion);
                                 }
                             }
 
                         }
-
-
-
-                        /*
-                        decimal salida = 0;
-
-                        if (i == 0)
-                        {
-                            HojaReporte.Cells[countRows-1, "A"] = renglon[i].ToString();
-                        }
-                        else if (!string.IsNullOrWhiteSpace(renglon[i]) && !decimal.TryParse(renglon[i], out salida))
-                        {
-                            descrpcion += Convert.ToString(renglon[i]) + " ";
-                            HojaReporte.Cells[countRows-1, "B"] = descrpcion;
-                        }
-                        else if (decimal.TryParse(renglon[i], out salida))
-                        {
-                            columnasImportes++;
-                            if (columnasImportes > 4) break;
-
-                            switch (columnasImportes)
-                            {
-                                case 1:
-                                    HojaReporte.Cells[countRows-1, "C"] = salida;
-                                    break;
-                                case 2:
-                                    HojaReporte.Cells[countRows-1, "D"] = salida;
-                                    break;
-                                case 3:
-                                    HojaReporte.Cells[countRows-1, "E"] = salida;
-                                    break;
-                                case 4:
-                                    HojaReporte.Cells[countRows-1, "F"] = salida;
-                                    break;
-                            }
-                        */
-
-
-
                     }
                 }
                
@@ -147,8 +112,40 @@ namespace reporte
 
                 Console.WriteLine(linea);
             }
+            PintarBordes();
         }
 
+
+        private void PintarBordes()
+        {
+
+            Range rango = HojaReporte.get_Range("A1", "G" + (countRows-2));
+
+            Range rangoDescripcion = HojaReporte.get_Range("C1", "C" + (countRows - 2));
+            rangoDescripcion.Columns.AutoFit();
+            rangoDescripcion.Font.Bold = true;
+
+            Range rangoEncabezados = HojaReporte.get_Range("A1", "F1");
+            rangoEncabezados.Font.Bold = true;
+            rangoEncabezados.WrapText = true;
+
+            HojaReporte.Cells[1, 7].WrapText = true;
+
+
+            Borders border = rango.Borders;
+            rango.Font.Size = 10;
+            border[XlBordersIndex.xlEdgeLeft].LineStyle = XlLineStyle.xlContinuous;
+            border[XlBordersIndex.xlEdgeTop].LineStyle = XlLineStyle.xlContinuous;
+            border[XlBordersIndex.xlEdgeBottom].LineStyle = XlLineStyle.xlContinuous;
+            border[XlBordersIndex.xlEdgeRight].LineStyle = XlLineStyle.xlContinuous;
+            border[XlBordersIndex.xlInsideHorizontal].LineStyle = XlLineStyle.xlDot;
+            border[XlBordersIndex.xlInsideVertical].LineStyle = XlLineStyle.xlContinuous;
+            border[XlBordersIndex.xlEdgeLeft].Weight = XlBorderWeight.xlThick;
+            border[XlBordersIndex.xlEdgeRight].Weight = XlBorderWeight.xlThick;
+            border[XlBordersIndex.xlEdgeTop].Weight = XlBorderWeight.xlThick;
+            border[XlBordersIndex.xlEdgeBottom].Weight = XlBorderWeight.xlThick;
+
+        }
         private void btnSalir_Click(object sender, EventArgs e)
         {
             Close();
@@ -156,14 +153,189 @@ namespace reporte
 
         private void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
         {
-            LeerTxt(ruta);
+            LeerTxt(ruta, rutaExcel);
         }
 
         private void backgroundWorker1_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
         {
             MessageBox.Show("Reporte generado correctamente.", "Aviso", MessageBoxButtons.OK, MessageBoxIcon.Information);
             btnCargarArchivo.Enabled = true;
+            ReporteApp.Visible = true;
             this.Focus();
+        }
+
+        private void ImportarArchivoCuentas()
+        {
+            openFileDialog1 = new OpenFileDialog();
+            openFileDialog1.ShowDialog(this);
+            ruta = openFileDialog1.FileName;
+            if (string.IsNullOrEmpty(ruta))
+            {
+                MessageBox.Show("No ha cargado ningun archivo. Intentelo de nuevo", "Advertencia", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+        }
+
+        private void buttonCargarExcel_Click(object sender, EventArgs e)
+        {
+            string error ="";
+            if (string.IsNullOrEmpty(txtHoja.Text)){
+                error = "la posición de la Hoja en la cual se realizará la generacion de las cuentas contables.";
+            }else if(string.IsNullOrEmpty(txtFilas.Text)) 
+            {
+                error = "el numero de la fila en la cual se iniciara a generar las cuentas contables.";
+            }else if (string.IsNullOrEmpty(txtColumna.Text))
+            {
+                error = "la columna en donde se situara la generación de las cuentas contables.";
+            }
+
+            if (!string.IsNullOrEmpty(error))
+            {
+                MessageBox.Show("Falto ingresar " + error, "Advertencia", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                    return;
+            }
+            ImportarExcel(Convert.ToInt32(txtHoja.Text), Convert.ToInt32(txtFilas.Text));
+        }
+
+        private void ImportarExcel(int hoja, int filas)
+        {
+            if (string.IsNullOrEmpty(ruta))
+            {
+                MessageBox.Show("No ha cargado el Archivo de Cuentas. Cargue el archivo e intentelo de nuevo", "Advertencia", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+            openFileDialog1 = new OpenFileDialog();
+            openFileDialog1.ShowDialog(this);
+            rutaExcel = openFileDialog1.FileName;
+            if (string.IsNullOrEmpty(rutaExcel))
+            {
+                MessageBox.Show("No ha cargado ningun archivo. Intentelo de nuevo", "Advertencia", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+            excelCargado = true;
+            SLDocument sl = new SLDocument(rutaExcel);
+            var lstHojas = sl.GetSheetNames();
+            Console.WriteLine(lstHojas[(hoja - 1)]);
+            string NombreHoja = /*SLDocument.DefaultFirstSheetName;*/lstHojas[(hoja - 1)];
+            sl.SelectWorksheet(NombreHoja);
+            Console.WriteLine(NombreHoja);
+            Console.WriteLine( sl.GetCurrentWorksheetName());  
+
+            countRows = filas;
+            sl.InsertRow(filas, 1);
+
+            sl.SetCellValue("A"+countRows, "CUENTA");
+            sl.SetCellValue("B"+countRows, "SUBCTA");
+            sl.SetCellValue("C"+countRows, "CONCEPTO");
+            sl.SetCellValue("D"+countRows, "SALDO SEGÚN BALANZA");
+            sl.SetCellValue("E"+countRows, "SALDO S/ AUDITORIA");
+            sl.SetCellValue("F"+countRows, "DIFERENCIA");
+            sl.SetCellValue("G"+countRows, "MARCA DE AUDITORIA");
+
+            
+            
+
+            SLStyle styleBordeGrueso = sl.CreateStyle();
+            SLStyle styleBordeDoteado = sl.CreateStyle();
+            SLStyle styleNegrita = sl.CreateStyle();
+            SLStyle styleThin = sl.CreateStyle();
+            SLStyle styleWrapText = sl.CreateStyle();
+
+            styleBordeGrueso.Border.LeftBorder.BorderStyle = BorderStyleValues.Medium;
+            styleBordeGrueso.Border.RightBorder.BorderStyle = BorderStyleValues.Medium;
+            styleThin.Border.RightBorder.BorderStyle = BorderStyleValues.Thin;
+            styleBordeDoteado.Border.BottomBorder.BorderStyle = BorderStyleValues.Dotted;
+            styleWrapText.SetWrapText(true);
+            styleWrapText.Alignment.Horizontal= HorizontalAlignmentValues.Center;
+            styleNegrita.SetFontBold(true);
+            sl.SetCellStyle(countRows, 1, countRows, 7, styleNegrita);
+            sl.SetCellStyle(countRows, 1, countRows, 7, styleWrapText);
+
+            int numeroRows = System.IO.File.ReadLines(ruta).Count() - 2;
+            sl.InsertRow(countRows+1, numeroRows);
+            int contadorLineas = countRows;
+            var txtCuentasContables = System.IO.File.ReadLines(ruta);
+            int posLineaCuentaContable = 0;
+            foreach (string linea in txtCuentasContables)
+            {
+                posLineaCuentaContable++;
+                if (posLineaCuentaContable <= 2)
+                {
+                    countRows++;
+                    continue;
+                }
+               // contadorLineas++;
+                
+                string ln = linea;
+                if (contadorLineas == numeroRows - 2) break;
+                else if (contadorLineas > 2)
+                {
+                    String[] renglon = ln.ToString().Split(' ');
+                    string descrpcion = "";
+                    int columnasImportes = 0;
+                    decimal salida = 0;
+                    for (int i = 0; i < renglon.Length; i++)
+                    {
+                        if (i == 0 && decimal.TryParse(renglon[i].ToString(), out salida))
+                        {
+                            columnasImportes = 0;
+                            var contenido = renglon[i].ToString();
+                            sl.SetCellValue("A" + (countRows-1), renglon[i].ToString());
+                        }
+                        else if (i == 0)
+                        {
+                            var contenido = renglon[i].ToString();
+                            sl.SetCellValue("B" + (countRows-1), renglon[i].ToString());
+                        }
+                        else
+                        {
+                            if (decimal.TryParse(renglon[i].ToString(), out salida))
+                            {
+                                columnasImportes++;
+                                if (columnasImportes == 4)
+                                {
+                                    var contenido = renglon[i].ToString();
+                                    sl.SetCellValue("D" + (countRows-1), renglon[i].ToString());
+                                    sl.SetCellValue("E" + (countRows-1), renglon[i].ToString());
+                                }
+                            }
+                            else
+                            {
+                                if (!string.IsNullOrWhiteSpace(renglon[i].ToString()))
+                                {
+                                    var contenido = renglon[i].ToString();
+                                    descrpcion += Convert.ToString(renglon[i]) + " ";
+                                    descrpcion = descrpcion.Contains('\a') ? descrpcion.Replace('\a', ' '): descrpcion;
+                                    sl.SetCellValue("C" + (countRows-1), (descrpcion));
+                                }
+                            }
+
+                        }
+                    }
+
+                    sl.SetCellStyle(countRows - 1, 1, countRows - 1, 7, styleBordeGrueso);
+                    sl.SetCellStyle(countRows - 1, 1, countRows - 1, 7, styleBordeDoteado);
+                    sl.SetCellStyle(countRows - 1, 1, countRows - 1, 3, styleNegrita);
+                }
+
+                Console.WriteLine(linea);
+                countRows++;
+            }
+
+
+
+
+
+
+
+            saveFileDialog1 = new SaveFileDialog();
+            saveFileDialog1.ShowDialog();
+
+            sl.SaveAs(saveFileDialog1.FileName/*+".xlsx"*/);
+
+
+
         }
     }
 }
